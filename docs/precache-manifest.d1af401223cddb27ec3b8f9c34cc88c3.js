ff6ff852874190dabad5ceb3af8227939b721ae7
Derek G Chen <derek@derekgc.com>
2020-06-16 20:43:59 -0400

    built - june-2020

---
+++ docs/precache-manifest.d1af401223cddb27ec3b8f9c34cc88c3.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f19a876a4622f48d138cbb6a14f8607b",
+    "url": "/trade-simulator/index.html"
+  },
+  {
+    "revision": "6f04ef05e4915eb8775c",
+    "url": "/trade-simulator/static/css/2.0d22d31e.chunk.css"
+  },
+  {
+    "revision": "700bb9b512d65d6aeccb",
+    "url": "/trade-simulator/static/css/main.b2498c36.chunk.css"
+  },
+  {
+    "revision": "6f04ef05e4915eb8775c",
+    "url": "/trade-simulator/static/js/2.7de4f22a.chunk.js"
+  },
+  {
+    "revision": "0842fae683d4cc5de0f68103909f3762",
+    "url": "/trade-simulator/static/js/2.7de4f22a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "700bb9b512d65d6aeccb",
+    "url": "/trade-simulator/static/js/main.80a4b5d6.chunk.js"
+  },
+  {
+    "revision": "d2f209dd34ec197d860e",
+    "url": "/trade-simulator/static/js/runtime-main.0dbbfe7c.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/trade-simulator/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/trade-simulator/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/trade-simulator/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/trade-simulator/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/trade-simulator/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/trade-simulator/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/trade-simulator/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/trade-simulator/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/trade-simulator/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/trade-simulator/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/trade-simulator/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/trade-simulator/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/trade-simulator/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/trade-simulator/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/trade-simulator/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/trade-simulator/static/media/outline-icons.ef60a4f6.woff"
+  }
+]);
